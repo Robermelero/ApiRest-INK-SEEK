b9e13cfdb7e01aa987f08766f57e10add6f61e01
picatostao <valver123@gmail.com>
2023-07-05 13:25:12 +0200

    addevent

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const errorHandling = require("./error/errorHandling");
 const userRouters = require("./routers/user.routers");
 const tiendaRouters = require("./routers/tienda.routers")
-
+const eventRoutes=require("./routers/event.routers")
 
 
 
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(userRouters);
 app.use(tiendaRouters);
+app.use(eventRoutes);
 app.use(function(req, res, next)
 {
     res.status(404).json({error: true,
---
+++ src/controller/events.Controller.js
@@ -0,0 +1,100 @@
+const { Pool } = require("../database");
+
+const postEvent=async(req,res)=>{
+    const id_user=req.params.id_user;
+    const {photo,title,date,place}=req.body; 
+    console.log("entrando");
+    try{
+        await Pool.query("INSERT INTO evento(photo,title,date,locate,id_user) VALUES (?,?,?,?,?) ",[photo,title,date,place,id_user])
+       respuesta={error:false, 
+        codigo:200, mensaje:
+        "Evento añadido"}
+        res.send(respuesta);
+        console.log("evento añadido");
+    }catch(error){
+        respuesta= {error:true, codigo:200, mensaje:"error al añadir evento",error}
+        console.log(error);
+        res.send(respuesta);
+        console.log("error al añadir evento");
+    }
+}
+
+const deleteEvent = async (req, res) => {
+    const { id_evento } = req.params;
+  
+    try {
+      await Pool.query('DELETE FROM evento WHERE id_evento = ?', [id_evento]);
+  
+      const respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: 'Evento eliminado',
+      };
+      res.send(respuesta);
+    } catch (error) {
+      const respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'Error al eliminar evento',
+        error: error.message,
+      };
+      console.log(error);
+      res.send(respuesta);
+    }
+  };
+
+  const getEvents = async (req, res) => {
+    try {
+      const events = await Pool.query('SELECT * FROM evento');
+  
+      const respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: 'Eventos obtenidos',
+        eventos: events,
+      };
+      res.send(respuesta);
+    } catch (error) {
+      const respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'Error al obtener eventos',
+        error: error.message,
+      };
+      console.log(error);
+      res.send(respuesta);
+    }
+  };
+
+  const searchEvent = async (req, res) => {
+    const { search } = req.query;
+  
+    try {
+      const events = await Pool.query(
+        `SELECT * FROM evento
+        INNER JOIN user ON evento.id_user = user.id_user
+        WHERE evento.title LIKE ? OR user.name LIKE ?`,
+        [`%${search}%`, `%${search}%`]
+      );
+  
+      const respuesta = {
+        error: false,
+        codigo: 200,
+  
+        mensaje: 'Eventos encontrados',
+        eventos: events,
+      };
+      res.send(respuesta);
+    } catch (error) {
+      const respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'Error al buscar eventos',
+        error: error.message,
+      };
+      console.log(error);
+      res.send(respuesta);
+    }
+  };
+
+  module.exports={postEvent,deleteEvent,getEvents,searchEvent}
