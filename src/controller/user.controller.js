6ce6cd5cd38b43ccc0e09cc0f3c7c653b2cef81d
albaCarranza <alba.carranza.g@gmail.com>
2023-07-11 16:26:16 +0200

    subida opiniones

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const { Pool } = require("../database");
 
 const postRegister = async (request, response) => {
@@ -95,4 +96,122 @@ const postLogin  = async (request,response) =>
        
       };
 
-module.exports = { postRegister, postLogin, getUserTatuadorInfo };
+      const deletePublicacion = async (request, response) => {
+        try{
+          let respuesta;
+          let sql = "DELETE FROM photo WHERE id_photo = '"+ request.body.id_photo +"'"
+          console.log(sql);
+
+          let [result] = await Pool.query(sql);
+          console.log(result);
+
+          respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: "publicacion borrada",
+            data_foto: result
+          };
+
+          response.send(respuesta)
+
+        }
+        catch (err){
+          console.log(err);
+        }
+      }
+
+const postOpinion = async (request, response) => {
+  try {
+   
+    let params = [];
+    let respuesta;
+    let sql2 = `INSERT INTO opiniones (emisor, receptor, comentario, puntuacion) VALUES (?,?,?,?)`;
+    console.log(sql2);
+    params = [
+      request.body.emisor,
+      request.body.receptor,
+      request.body.comentario,
+      request.body.puntuacion,
+    ]
+    
+    let [result] = await Pool.query(sql2, params);
+    respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: "opinion enviada",
+      data_opinion: result
+    };
+
+    console.log("hola :" + result);
+
+    response.send(respuesta)
+    console.log('esta es la respuesta: '+respuesta);
+
+  } catch (error) {
+    console.error(error);
+    response.send(error);
+  }
+
+};
+
+//esto lo hago para mostrar las opiniones en la vista tatuador
+
+const getOpiniones = async (request, response) => {
+  try {
+    let respuesta;
+    let params = []
+    // let sql = "SELECT * FROM opiniones WHERE receptor = '"+ request.body.receptor +"'";
+    // let sql = "SELECT * FROM opiniones";
+    let sql = "SELECT * FROM opiniones WHERE receptor = ?"
+console.log(sql);
+    params = [request.params.receptor]
+    let [result] = await Pool.query(sql, params);
+    console.log(result);
+    respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: 'Opiniones obtenidas',
+      data_opinion: result
+    };
+console.log(respuesta);
+    response.send(respuesta);
+    
+  } catch (error) {
+    console.error(error);
+    response.send(error);
+  }
+};
+
+//para borrar opinion que escribe el usuario
+
+const borrarOpinion = async (request, response) => {
+  try{
+    console.log(request.body);
+    console.log("consoleloooooooooooooog");
+    let respuesta;
+    let sql = "DELETE FROM opiniones WHERE id_opiniones = '"+ request.body.id_opiniones +"'"
+    console.log(sql);
+
+    let [result] = await Pool.query(sql);
+    console.log(result);
+
+    respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: "opinion borrada",
+      data_foto: result
+    };
+
+    response.send(respuesta)
+
+  }
+  catch (err){
+    console.log(err);
+  }
+}
+
+
+
+
+
+module.exports = { postRegister, postLogin, getUserTatuadorInfo, deletePublicacion, postOpinion, getOpiniones, borrarOpinion };
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -4,7 +4,13 @@ const userCtrl = require("../controller/user.controller");
 
 router.post("/registro", userCtrl.postRegister);
 router.post("/login",userCtrl.postLogin);
-router.get('/profile-tatuador-propia/:id_user', userCtrl.getUserTatuadorInfo)
+router.get('/profile-tatuador-propia/:id_user', userCtrl.getUserTatuadorInfo);
+router.delete('/profile-tatuador-propia', userCtrl.deletePublicacion);
+router.post('/estrellas', userCtrl.postOpinion);
+router.get('/opiniones/:receptor', userCtrl.getOpiniones);
+router.delete('/estrellas', userCtrl.borrarOpinion);
+
+
 
 
 module.exports = router;
