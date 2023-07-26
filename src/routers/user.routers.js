aebd59aef9b39cb77b6d1318a1c84633e8aba1cf
Roberto Melero <roberto.meleros@gmail.com>
2023-07-26 09:45:36 +0200

    final

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -7,7 +7,6 @@ router.post("/login",userCtrl.postLogin);
 router.put("/edit-profile-tatuador", userCtrl.editProfile);
 router.get("/descubrir-artista", userCtrl.getTatuadoresExplora)
 router.get('/profile-tatuador-propia/:id_user', userCtrl.getUserTatuadorInfo);
-router.get('/profile-tatuador-externa/:id_user', userCtrl.getUserTatuadorInfo);
 router.delete('/profile-tatuador-propia', userCtrl.deletePublicacion);
 
 
